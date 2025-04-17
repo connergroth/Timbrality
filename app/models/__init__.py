d434ab8a38506ba33937c6e647395132c45a62e5
Conner Groth <connergroth@gmail.com>
2025-04-16 19:29:45 -0600

    refactored file structure

--- app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .users.user import User
-from .tracks.track import Track
-from .album import Album
-from .artist import Artist
-from .recommendation import Recommendation
-
-__all__ = ["User", "Track", "Album", "Artist", "Recommendation"]
